94d6367f1458b39da47a8ce136f07cad52c727f2
penguin <anton.lobkov.mtb@gmail.com>
2022-10-26 21:35:55 +0300

    але воркаем

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,7 @@ module.exports = {
         "react"
     ],
     "rules": {
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     }
 }
